f7da5fc9d8f3c0e06ddffef13bb82f2da2dc3974
gulaitapidigoreng <ghifari.shinichi31@gmail.com>
2026-06-13 21:11:17 +0700

    Models layer refactoring actions: Created explicit Enums to represent several restricted sets of values like Status and Payment Method on RepairOrder.cs, and LastPaymentMethod on Customer.cs. And then updated the models to use these new types

---
+++ AutoServiceApp/Models/DomainEnums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoServiceApp.Models;
+
+public enum OrderStatus
+{
+    New,
+    InProgress,
+    Completed,
+    Cancelled
+}
+
+public enum PaymentType
+{
+    Cash,
+    Card,
+    BankTransfer
+}
