65f0a0909596b589e60290116239dd15d1f31128
gulaitapidigoreng <ghifari.shinichi31@gmail.com>
2026-06-13 21:30:06 +0700

    Defetct: Data Clumps Models Layer refactoring actions: Extracted Phone, Email, and Address in Customer.cs because those three data points are almost always used together. Stored those at "DataClumps.cs"

---
+++ AutoServiceApp/Models/DataClumps.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoServiceApp.Models;
+
+public record ContactInfo(string Phone, string Email, string Address);
